b96b8e034fc8fa0b2235b642023af667022190f8
Tali Ngatuvai <Tngatuvai85@gmail.com>
2020-07-07 22:52:37 -0600

    Another one

---
+++ JavaScript/15.js
@@ -0,0 +1,16 @@
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+// Solution: 
+
+function findLongest(array){
+    let num = '0';
+    for(let i=0; i<array.length; i++) {
+      let string = array[i].toString();
+      if(string.length > num.length) {
+        num = string;
+      }
+    }
+    return parseInt(num);
+  }
